e12e81ecb35a25fbc6fc0c75e1c95a923147ab1b
Vytautas Perliba <vperliba@kayak.com>
2018-10-24 09:08:11 +0300

    change tip

--- src/server.js
+++ src/server.js
@@ -55,7 +55,7 @@ const questions = {
     answer: '11',
     question: 'You’re taking care of a friend’s house whilst he’s on vacation. In one room you pull the chain on a ceiling fan, and when it doesn’t respond you realize the house has temporarily lost power. Doh! Unfortunately, you have to leave now, and you’ll be away for several days.\n\nYou know that the fan was in the “off” position before you pulled the chain, and that pulling the chain successively will cycle it through its remaining settings ("off", “high”, “medium”, etc.). You don’t know how many settings there are, but you are sure there aren’t more than four (including "off").\n\nHow many times more do you have to pull the chain to ensure that the fan will be in the “off” position when power is restored?',
     hint: 'Try finding a Lowest Common Multiple',
-    nextStateTip: 'Come and relax with me, under a palm tree!'
+    nextStateTip: 'Come and SMALL TALK with me, under a palm tree!'
   },
   '215d6520-040c-4099-8f32-dade1ebd390a': {
     state: 5,
